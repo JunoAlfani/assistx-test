e6f144d6015993a52e7e76f8017652bb0a92bfc9
Juno <junoalfani@gmail.com>
2025-11-29 15:35:47 +0700

    upload assistx-test files

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return response()->json(['msg' => 'Hello World'], 200)
+                     ->header('Content-Type', 'application/json');
+});
